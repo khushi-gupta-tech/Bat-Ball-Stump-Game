ca09d1ad0730184cef7ef61acd8ef312782c75e1
khushi gupta <kg834208@gmail.com>
2025-07-27 00:27:17 +0530

    update the files

--- script.js
+++ script.js
@@ -2,29 +2,49 @@ const buttons = document.querySelectorAll("button");
 const Yours = document.querySelector("#yours");
 const computer = document.querySelector("#computer");
 const result = document.querySelector("h3");
+const scores = document.querySelector("#score");
+const reset = document.querySelector("#reset");
 
+let score = {
+    win :0,
+    lose:0,
+    tie:0
+}
+function sco(win,lose,tie){
+    scores.innerHTML = `Win: ${win} <br>
+                        Lose: ${lose} <br>
+                        Tie: ${tie}`
+}
 function compare(userChoice, computerChoice) {
     if (userChoice === computerChoice) {
+        score.tie++;
         result.innerHTML = "Match Draw!";
     } 
     else if (userChoice === "Bat" && computerChoice === "Ball") {
+        score.win++;
         result.innerHTML = "You Win, your bat beats ball!";
     } 
     else if (userChoice === "Bat" && computerChoice === "Stump") {
+        score.lose++;
         result.innerHTML = "You Lose,Stump beats your bat!";
     } 
     else if (userChoice === "Ball" && computerChoice === "Bat") {
+        score.lose++;
         result.innerHTML = "You Lose,bat beats your ball!";
     } 
     else if (userChoice === "Ball" && computerChoice === "Stump") {
+        score.win++;
         result.innerHTML = "You Win, your ball beats stump";
     } 
     else if (userChoice === "Stump" && computerChoice === "Bat") {
-        result.innerHTML = "You Win, your stump beats bat!";
+        score.win++;
+        result.innerHTML = "You Win, your stump beats bat !";
     } 
     else if (userChoice === "Stump" && computerChoice === "Ball") {
+        score.lose++;
         result.innerHTML = "You Lose,ball beats your stump!";
     }
+    sco(score.win,score.lose,score.tie)
 }
 
 function playGame(userChoice) {
@@ -44,3 +64,14 @@ buttons.forEach((b) => {
     playGame(userChoice);
   });
 });
+
+reset.addEventListener('click',()=>{
+    Yours.innerHTML = '';
+    computer.innerHTML = '';
+    result.innerHTML = '';
+    scores.innerHTML = '';
+    
+    score.win = 0;
+    score.lose = 0;
+    score.tie = 0;
+})
